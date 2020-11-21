c9dfa13a61c2a69951110c3fd23d179c6a556ad6
Denae716 <denaebrown73@yahoo.com>
2020-11-21 12:34:26 -0500

    part 2

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+
+
+// part 1
 console.log("Celsius", "Farenheit");
 for (let c = 0; c < 101; c++) {
     let f = (c * (9 / 5) + 32)
     console.log(c, f);
 
 }
+
+// part 2
+let userInput = +prompt("Pick a number")
+let total = 0;
+let n = 0;
+do {
+    total += n;
+     n++;
+}
+ while( n < userInput + 1 )
+ console.log(total)
